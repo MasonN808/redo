c18d77e3478da2b9afbec8652541de78ffe39bac
MasonN808 <mason.naka@gmail.com>
2024-05-14 14:21:14 -0400

    initialized KAN DQN agents

--- src/agent.py
+++ src/agent.py
@@ -1,7 +1,8 @@
 import torch
 import torch.nn as nn
 import torch.nn.functional as F
-
+from tfkan.layers import DenseKAN
+import tensorflow as tf
 
 class QNetwork(nn.Module):
     """Base class for different QNetwork configurations."""
@@ -14,6 +15,48 @@ class QNetwork(nn.Module):
         raise NotImplementedError("Each model must implement its own forward method.")
 
 class QNetworkNature(QNetwork):
+    """Basic nature DQN agent."""
+
+    def __init__(self, env):
+        super().__init__(env)
+        self.conv1 = nn.Conv2d(4, 32, 8, stride=4)
+        self.conv2 = nn.Conv2d(32, 64, 4, stride=2)
+        self.conv3 = nn.Conv2d(64, 64, 3, stride=1)
+        self.fc1 = nn.Linear(3136, 512)
+        self.q = nn.Linear(512, self.n_actions)
+
+    def forward(self, x):
+        x = F.relu(self.conv1(x / 255.0))
+        x = F.relu(self.conv2(x))
+        x = F.relu(self.conv3(x))
+        x = torch.flatten(x, start_dim=1)
+        x = F.relu(self.fc1(x))
+        x = self.q(x)
+        return x
+
+# TODO: Finish. This is utilizing keras but not sure if this will change the entire code base.
+class QNetworkKAN(QNetwork):
+    """KAN DQN agent."""
+    # See https://github.com/ZPZhou-lab/tfkan?tab=readme-ov-file#how-to-use for KAN tensorflow implementation
+    def __init__(self, env):
+        super().__init__(env)
+        n_input_channels = env.observation_space.shape[1]
+
+        self.model = tf.keras.models.Sequential([
+            # DenseKAN(n_input_channels),
+            DenseKAN(64),
+            DenseKAN(64),
+            DenseKAN(self.n_actions)
+        ])
+
+        self.model.build(input_shape=(None, n_input_channels))
+
+    def forward(self, x):
+        x = self.model(x)
+        return x
+
+    
+class QNetworkKANConv(QNetwork):
     """Basic nature DQN agent."""
 
     def __init__(self, env):
